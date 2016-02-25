eb86b078c1b27617fe31496e55b35509eabec287
ristuo <risto.tuomainen@helsinki.fi>
2016-02-25 12:10:37 +0200

    fixy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,5 @@ package dm
 object Main {
     def main(args: Array[String]) {
         val data = DataReader.readCsv("data.csv")
-        //Week5.toCategories(data)
-        Group2.grouping(data)
     }
 }
